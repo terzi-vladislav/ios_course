8d007999006192c210f461c099945e338be57b11
terzi-vladislav <terzi.va@phystech.edu>
2020-03-11 18:11:09 +0300

    Refactored storyboards, created custom classes for ViewControllers

---
+++ Instaclone/SearchViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SearchViewController.swift
+//  Instaclone
+//
+//  Created by Terzi Vladislav on 11.03.2020.
+//  Copyright © 2020 Terzi Vladislav. All rights reserved.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
