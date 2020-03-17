2fc50356988401de95a5b4f13bc60a35bf629626
terzi-vladislav <terzi.va@phystech.edu>
2020-03-18 01:18:28 +0300

    modified some files

--- Instaclone/HomeViewController.swift
+++ Instaclone/HomeViewController.swift
@@ -7,13 +7,26 @@
 //
 
 import UIKit
+import Alamofire
 
 class HomeViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        execute()
 
         // Do any additional setup after loading the view.
+    }
+    
+    func execute() {
+        let headers = HTTPHeaders([HTTPHeader.authorization("Token 650c296a8e578f02db54481f393afdb08cc216c8")])
+        let url = "https://cluster.likeup.me/api/get_hashtag_feed/?hashtag=mipt"
+
+        AF.request(url, method: .get, parameters: nil, encoding: URLEncoding.default, headers: headers, interceptor: nil).responseJSON
+            {
+                response in
+            print(response)
+        }
     }
     
 
